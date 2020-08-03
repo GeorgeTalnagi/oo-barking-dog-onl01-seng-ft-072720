77bc05c877937a2e5e04a7530977cad1a4963f2c
George Talnagi <gtalnagi@yahoo.com>
2020-08-03 19:29:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,10 +4,10 @@ def bark
   puts "woof!"
 end 
 def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
  
   def name
-    this_dogs_name
+    @this_dogs_name
   end
 end 
